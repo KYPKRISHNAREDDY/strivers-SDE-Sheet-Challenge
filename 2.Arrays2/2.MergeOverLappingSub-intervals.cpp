5fe5c0a449adafe65d73745d4d51a69da30a4dc7
KYPKRISHNAREDDY <kypkrishna777@gmail.com>
2023-06-13 22:13:44 +0530

    Day2:final Commit

--- 2.Arrays2/2.MergeOverLappingSub-intervals.cpp
+++ 2.Arrays2/2.MergeOverLappingSub-intervals.cpp
@@ -31,6 +31,21 @@ vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
 }
 
 
+//Optimized Approach
+vector<vector<int>> merge(vector<vector<int>> &arr){
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
+
+    vector<vector<int>> ans;
+    for(int i=0;i<n;i++){
+        if(ans.empty || arr[i][0]>ans.back()[1])
+            ans.push_back(arr[i]);
+        else{
+            ans.back()[1]=max(ans.back()[1],arr[i][1]);
+        }
+    }
+    return ans;
+}
 int main() {
     vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
     vector<vector<int>> ans = mergeOverlappingIntervals(arr);
